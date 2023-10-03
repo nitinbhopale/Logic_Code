df0df0c4d7146b54c55e6eef1d696b87d44deafd
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-03 21:08:24 +0530

    cmt

--- Program20.c
+++ Program20.c
@@ -1,10 +1,10 @@
-// Accept two numbers from user and return largest number 
+// Accept two numbers from user and return smallest number 
 
 #include<stdio.h>
 
-int Maximum(int iNo1, int iNo2)
+int Minimum(int iNo1, int iNo2)
 {
-    if(iNo1>iNo2)
+    if(iNo1<iNo2)
     {
         return iNo1;
     }else {
@@ -24,8 +24,8 @@ int main()
     printf("Enter the second Number\n");
     scanf("%d",&iValue2);
 
-    iRet = Maximum(iValue1,iValue2);
+    iRet = Minimum(iValue1,iValue2);
 
-    printf("Largest Number is : %d\n",iRet);
+    printf("Smallest Number is : %d\n",iRet);
     return 0;
 }
