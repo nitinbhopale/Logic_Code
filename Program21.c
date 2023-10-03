37b730cb8d1cdf781435b9c9427ae504ee0e3751
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-03 21:14:43 +0530

    cmt

---
+++ Program21.c
@@ -0,0 +1,33 @@
+// accept 3 numbers from user and return average
+
+#include<stdio.h>
+float Average(int iNo1, int iNo2, int iNo3)
+{
+    float Average = 0.0f;
+    Average = ((float)iNo1 + (float)iNo2 + (float)iNo3)/3;
+    return Average;
+}
+
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+    int iNo3 = 0;
+    float fRet = 0.0f;
+    printf("Enter the First Number\n");
+    scanf("%d",&iNo1);
+
+    
+    printf("Enter the Second Number\n");
+    scanf("%d",&iNo2);
+
+    
+    printf("Enter the Third Number\n");
+    scanf("%d",&iNo3);
+
+    fRet = Average(iNo1,iNo2,iNo3);
+
+    printf("Average is : %f",fRet);
+
+    return 0;
+}
