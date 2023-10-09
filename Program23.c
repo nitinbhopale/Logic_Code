6d1b2461fb7d5040b31524e0a5e1234a57c461df
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-09 19:09:23 +0530

    cmt

---
+++ Program23.c
@@ -0,0 +1,17 @@
+// print 5 times on screen
+#include<stdio.h>
+
+void Display()
+{
+    for(int i = 0; i < 5; i++)
+    {
+       printf("Jay Ganesh\n");      
+    }
+}
+
+int main()
+{
+    Display();
+    
+    return 0;
+}
