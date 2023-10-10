a4be81bb58be2090f42a373384f3a96cafde30a9
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-10 19:42:16 +0530

    amt

--- Program36.c
+++ Program36.c
@@ -1,9 +1,29 @@
-// 
+// accept the number from user and return the addition of factor
 
 #include<stdio.h>
 
+int FactorAddition(int iNo)
+{
+    int Ans = 0;
+    int i = 0;
+    for(i=1;i<=(iNo/2);i++)
+    {
+        if(iNo%i==0)
+        {
+            Ans += i;
+        }
+    }
+    return Ans;
+}
 int main()
 {
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter the Number\n");
+    scanf("%d",&iValue);
 
+    iRet = FactorAddition(iValue);
+
+    printf("The Addition of its factor is : %d\n",iRet);
     return 0;
 }
