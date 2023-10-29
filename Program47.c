66b2d7439f97d5bbfdc1c23bbaed91dcd464e10b
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-29 08:50:08 +0530

    commit

---
+++ Program47.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int ReverseNumber(int iNo)
+{
+    int iRev = 0;
+    int iDigit = 0;
+    
+    while (iNo!=0)
+    {
+        iDigit = iNo%10;
+        
+        iNo = iNo/10;
+        
+        iRev = iRev * 10 +iDigit;
+    }
+    return iRev;
+    
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter the number\n");
+    scanf("%d",&iValue);
+
+    iRet = ReverseNumber(iValue);
+
+    printf("%d",iRet);
+    return 0;
+}
