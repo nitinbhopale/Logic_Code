722747b5f7246f3c8a3cc27d2384cb56ffc73733
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-31 19:10:16 +0530

    ok

---
+++ Program66.c
@@ -0,0 +1,21 @@
+/*
+              1 2 3 4 5 6 
+    Pattern : * * * * * * 
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+    for(int i=1;i<=6;i++)
+    {
+        printf("*\t");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
