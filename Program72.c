25b5368e8d66991e0c759e57d399d5d7b61c8d6a
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-31 19:38:50 +0530

    commit

---
+++ Program72.c
@@ -0,0 +1,29 @@
+/* 
+
+    Input : 4
+    Output : 4  3   2   1    
+     
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    
+    for(int i= iNo;i>0;i--)
+    {
+        printf("%d\t",i);
+    } 
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the Count\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
