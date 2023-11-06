b92dde56b81b88a70b700bf9b025c8f0f73caa49
nitinbhopale <nitinbhopale53@gmail.com>
2023-11-06 19:35:42 +0530

    commit

--- Program94.c
+++ Program94.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-    
+    char ch = '\0';
+
+    printf("Enter the Character : \n");
+    scanf("%c",&ch);
+
+    printf("ASCII Value of the character in DECIMAL is : %d\n",ch);
+    printf("ASCII Value of the character in OCTAL is : %o\n",ch);
+    printf("ASCII Value of the character is HEXADECIMAL : %x\n",ch);
+
     return 0;
 }
---
+++ Program96.c
@@ -0,0 +1,42 @@
+/*
+    According character from user and check it capital or not 
+*/
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckCapital(char cValue)
+{
+
+    if((cValue >= 65) && (cValue<=90))
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+
+int main()
+{
+    char ch = '\0';
+    bool bRet = false;
+
+
+    printf("Enter the Character\n");
+    scanf("%c",&ch);
+
+    bRet = CheckCapital(ch);
+
+    if(bRet == true)
+    {
+        printf("Its a capital letter\n");
+    } 
+    else 
+    {
+        printf("Its not a capital letter\n");
+    }
+
+    return 0;
+}
