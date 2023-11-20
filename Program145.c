9366fb3e51573ed3823a9ef46ac775536e36fd2d
nitinbhopale <nitinbhopale53@gmail.com>
2023-11-20 22:53:37 +0530

    commit

--- Program145.c
+++ Program145.c
@@ -16,7 +16,6 @@ void InsertFirst(PPNODE Head, int No)
 {
     // Step 1 : Allocate memory for node
     PNODE newn = NULL;
-
     newn = (PNODE)malloc(sizeof(NODE));
 
     // Step 2 : Initialise the node
@@ -32,20 +31,32 @@ void InsertFirst(PPNODE Head, int No)
     {
         newn -> next = *Head; 
         *Head = newn;
-    }
-    
+    } 
 }
 
+void Display(PNODE Head)
+{
+    printf("Elements of Linked List are : \n");
+
+    while(Head != NULL)
+    {
+        printf("| %d | -> ",Head->Data);
+        Head = Head -> next;
+    }
+    printf("Null \n");
+}
 
 
 int main()
 {
     PNODE First = NULL;
     
-    InsertFirst(&First, 51);
-    InsertFirst(&First, 21);
-    InsertFirst(&First, 11);
 
+    InsertFirst(&First, 51); // Insert first (60,51);
+    InsertFirst(&First, 21); // Insert first (60,21);
+    InsertFirst(&First, 11); // Insert first (60,11);
+    
+    Display(First); 
 
     return 0;
 }
--- Program147.c
+++ Program147.c
@@ -16,7 +16,6 @@ void InsertFirst(PPNODE Head, int No)
 {
     // Step 1 : Allocate memory for node
     PNODE newn = NULL;
-
     newn = (PNODE)malloc(sizeof(NODE));
 
     // Step 2 : Initialise the node
@@ -32,20 +31,45 @@ void InsertFirst(PPNODE Head, int No)
     {
         newn -> next = *Head; 
         *Head = newn;
-    }
-    
+    } 
 }
 
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d | -> ",Head->Data);
+        Head = Head -> next;
+    }
+    printf("Null \n");
+}
+
+int Count(PNODE Head)
+{
+    int iCnt = 0;
+    while(Head != NULL)
+    {
+        iCnt++;
+        Head = Head -> next;
+        
+    }
+    return iCnt;    
+}
 
 
 int main()
 {
     PNODE First = NULL;
+    int iRet = 0;
+
+    InsertFirst(&First, 101);
+    InsertFirst(&First, 51); // Insert first (60,51);
+    InsertFirst(&First, 21); // Insert first (60,21);
+    InsertFirst(&First, 11); // Insert first (60,11);
     
-    InsertFirst(&First, 51);
-    InsertFirst(&First, 21);
-    InsertFirst(&First, 11);
+    Display(First); 
+    iRet = Count(First);
 
-
+    printf("Number of Nodes are : %d\n",iRet);
     return 0;
 }
