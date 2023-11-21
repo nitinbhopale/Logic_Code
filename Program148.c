6f47441996506362ee7b90ca7e6eac0511d6eff6
nitinbhopale <nitinbhopale53@gmail.com>
2023-11-21 19:09:16 +0530

    cmt

---
+++ Program148.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE Head, int No)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = No;
+    newn->next = NULL;
+
+    if(*Head==NULL) // Linked List is empty
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->next = *Head;
+        *Head = newn;
+    }
+}
+
+
+void Display(PNODE Head)
+{
+    printf("Contents of Linked List : \n");
+    while(Head!=NULL)
+    {
+        printf("|%d|->",Head->data);
+        Head = Head->next;
+    }
+    printf("  NULL\n");
+}
+
+int Count(PNODE Head)
+{
+    int iCnt = 0;
+    while(Head!=NULL)
+    {
+        iCnt++;
+        Head = Head->next;
+    }
+    return iCnt;
+}
+int main()
+{
+    PNODE First = NULL;
+    int iRet = 0;
+
+    InsertFirst(&First,51);
+    InsertFirst(&First,21);
+    InsertFirst(&First,11);
+    InsertFirst(&First,1);
+
+
+
+    iRet = Count(First);
+
+    Display(First);
+    printf("The Number of element in Linked List is : %d",iRet);
+    return 0;
+}
